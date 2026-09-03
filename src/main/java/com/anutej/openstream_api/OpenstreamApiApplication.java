9d6eb229f58f775fb378b58a811373f59bb43467
Anutej Kardele <58202649+anutej-kardele@users.noreply.github.com>
2026-09-02 21:24:42 -0400

    first commit

---
+++ src/main/java/com/anutej/openstream_api/OpenstreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.anutej.openstream_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenstreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenstreamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anutej/openstream_api/OpenstreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anutej.openstream_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenstreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
